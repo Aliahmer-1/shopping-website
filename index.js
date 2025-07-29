3ee1321c7d8818e8ff84b15f831ba170e69b1013
Aliahmer-1 <aliahmer2434@gmail.com>
2025-07-29 15:34:33 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-let productId = new URLSearchParams(window.location.search)
-let id = productId.get("id");
+async function getData() {
+  let response = await fetch("https://dummyjson.com/products");
+  let data = await response.json();
+  let { products } = data;
+  let cardContainer = document.getElementById("cards");
 
-async function getProductDetail() {
-    let response = await fetch(`https://dummyjson.com/products/${id}`);
-    let data = await response.json();
+  products.forEach((product) => {
+    let { title, description, price, images,  id } = product;
 
-    console.log(data);
-    let { title, description, category, images, price, rating, stock, warrantyInformation } = data;
-
-    let pImage = document.getElementById('product-detail-image');
-    let pName = document.getElementById('product-name');
-    let pCategory = document.getElementById('product-category');
-    let pPrice = document.getElementById('product-price');
-    let pDescription = document.getElementById('product-description');
-    let pFeature1 = document.getElementById('product-feature-1');
-    let pFeature2 = document.getElementById('product-feature-2');
-    let pFeature3 = document.getElementById('product-feature-3');
-
-   
-    pImage.innerHTML = "";
-    pName.innerHTML = "";
-    pCategory.innerHTML = "";
-    pPrice.innerHTML = "";
-    pDescription.innerHTML = "";
-    pFeature1.innerHTML = "";
-    pFeature2.innerHTML = "";
-    pFeature3.innerHTML = "";
-
-  images.forEach((image, index) => {
-  pImage.innerHTML += `
-    <div class="carousel-item ${index === 0 ? 'active' : ''}">
-      <img class="d-block w-100" src="${image}" alt="Product image ${index + 1}">
-    </div>
-  `;
-});
-
-    pName.innerHTML = title;
-    pCategory.innerHTML = category;
-    pPrice.innerHTML = `$${price}`;
-    pDescription.innerHTML = description;
-    pFeature1.innerHTML = `Rating: ${rating} <br> Stock: ${stock}`;
-    pFeature2.innerHTML = `Warranty: ${warrantyInformation || 'N/A'}`;
-    pFeature3.innerHTML = `Category: ${category}`;
+    cardContainer.innerHTML += `
+      <div class="col-md-4 mb-4">
+        <div class="card h-100 shadow-sm">
+          <img src="${images[0]}" class="card-img-top" alt="${title}">
+          <div class="card-body">
+            <h5 class="card-title"><a href="./card-detail/index.html?id=${id}" target="_blank" class="text-decoration-none">${title}</a></h5>
+            <p class="card-text">${description.slice(0, 60)}...</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">$${price}</small>
+          </div>
+        </div>
+      </div>
+    `;
+  });
 }
 
-getProductDetail();
+
+
+getData();
